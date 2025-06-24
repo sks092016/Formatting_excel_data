6997882e904d28250796234996292788c9c4ec41
sks092016 <subhashsoni403@gmail.com>
2025-06-24 09:04:53 +0530

    Completed the Tarana BoQ four Primary Sheets

--- generating_row_presurvey.py
+++ generating_row_presurvey.py
@@ -6,7 +6,7 @@ cols = ['SrNo', 'FROM  AND TO GP','NHSHNo','RoadWidth','RowBoundaryLmt','KMStone
 
 row_in_dep = pd.DataFrame(columns=cols)
 
-porsa_ds = pd.read_excel('excel_files/mapped_output_Porsa.xlsx', sheet_name='Details Sheet')
+_ds = pd.read_excel('excel_files/Tarana Block/Tarana-BoQ.xlsx', sheet_name='Details Sheet')
 
 
 def process_kms_photo(value):
@@ -17,17 +17,23 @@ def process_kms_photo(value):
 
 
 def find_lat(value):
-    cords = value.strip('POINT()').split(' ')
+    try:
+        cords = value.strip('POINT()').split(' ')
+    except:
+        return 'NA'
     return cords[1]
 
 
 def find_long(value):
-    cords = value.strip('POINT()').split(' ')
+    try:
+        cords = value.strip('POINT()').split(' ')
+    except:
+        return 'NA'
     return cords[0]
 
 
 def road_xing_req(value):
-    if 'road' in str(value).lower():
+    if 'road' in str(value).lower() or 'rod' in str(value).lower():
         return 'Yes'
     else:
         return 'NA'
@@ -49,11 +55,17 @@ def finding_utility_lhs(row):
         elif 'bsnl' in str(row['POINT NAME']).lower():
             return 'BSNL'
         elif 'gas' in str(row['POINT NAME']).lower():
-            return 'Gas PipeLine'
+            return 'Gas PipeLine ' + str(row['POINT NAME'])
         elif 'hpcl' in str(row['POINT NAME']).lower():
             return 'HPCL Pipeline'
         elif 'iocl' in str(row['POINT NAME']).lower():
             return 'IOCL Pipeline'
+        elif 'railway' in str(row['POINT NAME']).lower():
+            return 'railway'
+        elif 'petrol' in str(row['POINT NAME']).lower():
+            return 'Petroleum Pipeline ' + str(row['POINT NAME'])
+        elif 'gail' in str(row['POINT NAME']).lower():
+            return 'Gail Xing'
     else:
         return 'NA'
 
@@ -67,18 +79,24 @@ def finding_utility_rhs(row):
         elif 'bsnl' in str(row['POINT NAME']).lower():
             return 'BSNL'
         elif 'gas' in str(row['POINT NAME']).lower():
-            return 'Gas PipeLine'
+            return 'Gas PipeLine ' + str(row['POINT NAME'])
         elif 'hpcl' in str(row['POINT NAME']).lower():
             return 'HPCL Pipeline'
         elif 'iocl' in str(row['POINT NAME']).lower():
             return 'IOCL Pipeline'
+        elif 'railway' in str(row['POINT NAME']).lower():
+            return 'railway'
+        elif 'petrol' in str(row['POINT NAME']).lower():
+            return 'Petroleum Pipeline ' + str(row['POINT NAME'])
+        elif 'gail' in str(row['POINT NAME']).lower():
+            return 'Gail Xing'
     else:
         return 'NA'
 
 
 def utility_checked(row):
     value = row['POINT NAME']
-    if any(sub in value.lower() for sub in ['rjil', 'airtel', 'bsnl', 'gas', 'hpcl', 'iocl']):
+    if any(sub in value.lower() for sub in ['rjil', 'airtel', 'bsnl', 'gas', 'hpcl', 'iocl', 'adani', 'railway', 'petrol', 'gail']):
         return "Route Marker"
     else:
         return "NA"
@@ -86,28 +104,32 @@ def utility_checked(row):
 
 def road_authority(row):
     if 'pmgy' in str(row['AUTHORITY NAME']).lower():
-        return f"PMGSY-{str(row['ROAD NAME']).upper()}(MPRRDA)"
-    elif 'pwd' in str(row['AUTHORITY NAME']).lower():
-        return f"PWD-(Morena)"
+        return f"PMGSY-{str(row['ROAD NAME']).upper()} (MPRRDA)"
+    elif 'pwd' in str(row['AUTHORITY NAME']).lower() or 'sh' in str(row['AUTHORITY NAME']).lower() or 'odr' in str(row['AUTHORITY NAME']).lower():
+        return f"PWD-(Ujjain)"
     elif 'NHAI' in str(row['AUTHORITY NAME']).lower():
-        return f"NHAI-Morena"
+        return f"NHAI-Ujjain"
+    elif 'nagar' in str(row['AUTHORITY NAME']).lower():
+        return f"Nagar Parishad-Tarana"
     else:
         return row['AUTHORITY NAME']
 
 
 def road_auth_add(row):
     if 'pmgy' in str(row['AUTHORITY NAME']).lower():
-        return f"General Manager, (PMGSY) MPRRDA, Morena"
-    elif 'pwd' in str(row['AUTHORITY NAME']).lower():
-        return f"Executive Engineer,  PWD-(Morena)"
+        return f"General Manager, (PMGSY) MPRRDA, Ujjain"
+    elif 'pwd' in str(row['AUTHORITY NAME']).lower() or 'sh' in str(row['AUTHORITY NAME']).lower() or 'odr' in str(row['AUTHORITY NAME']).lower():
+        return f"Executive Engineer,  PWD-(Ujjain)"
     elif 'NHAI' in str(row['AUTHORITY NAME']).lower():
-        return f"Project Director, NHAI-Morena"
+        return f"Project Director, NHAI-Ujjain"
+    elif 'nagar' in str(row['AUTHORITY NAME']).lower():
+        return f"CMO, Nagar Parishad-Tarana"
     else:
-        return f"{row['AUTHORITY NAME']}, Morena"
+        return f"{row['AUTHORITY NAME']}, Ujjain"
 
 
 def nearest_landmark(value):
-    if any(sub in value.lower() for sub in ['school', 'college', 'temple']):
+    if any(sub in value.lower() for sub in ['school', 'college', 'temple', 'gp', 'gram', 'kms']):
         return value.upper()
     else:
         return "NA"
@@ -128,16 +150,22 @@ def calculate_structure(row):
 
 def structure_xing_lat(row):
     if any(sub in str(row['POINT NAME']).lower() for sub in ['cul', 'bri', 'canal']):
-        return str(row['CROSSING_START']).strip('POINT()').split(' ')[1]
+        try:
+            return str(row['CROSSING_START']).strip('POINT()').split(' ')[1]
+        except:
+            return 'NA'
     else:
-        return ""
+        return "NA"
 
 
 def structure_xing_long(row):
     if any(sub in str(row['POINT NAME']).lower() for sub in ['cul', 'bri', 'canal']):
-        return str(row['CROSSING_END']).strip('POINT()').split(' ')[0]
+        try:
+            return str(row['CROSSING_END']).strip('POINT()').split(' ')[0]
+        except:
+            return 'NA'
     else:
-        return ""
+        return "NA"
 
 
 def landmark_lat(value):
@@ -156,50 +184,51 @@ def landmark_long(value):
 
 def landmark(value):
     if any(sub not in str(value).lower() for sub in
-           ['cul', 'bri', 'canal', 'rjil', 'airtel', 'bsnl', 'gas', 'hpcl', 'iocl']):
+           ['cul', 'bri', 'canal', 'rjil', 'airtel', 'bsnl', 'gas', 'hpcl', 'iocl', 'railway', 'petroleum', 'gail', 'rod', 'road', 'kacha']):
         return str(value).upper()
     else:
         return ""
 
 
-row_in_dep['SrNo'] = porsa_ds['SPAN_CONTINUITY']
-row_in_dep['FROM  AND TO GP'] = porsa_ds['ROUTE NAME']
-row_in_dep['NHSHNo'] = porsa_ds['ROUTE KMS PHOTO']
-row_in_dep['RoadWidth'] = porsa_ds['ROAD WIDTH(m)']
+row_in_dep['SrNo'] = _ds['SPAN_CONTINUITY']
+row_in_dep['FROM  AND TO GP'] = _ds['ROUTE NAME']
+# row_in_dep['NHSHNo'] = _ds['ROUTE KMS PHOTO']
+row_in_dep['NHSHNo'] = _ds['ROAD CHAINAGE']
+row_in_dep['RoadWidth'] = _ds['ROAD WIDTH(m)']
 row_in_dep['RowBoundaryLmt'] = 'NA'
-row_in_dep['KMStoneFromA'] = porsa_ds['ROAD CHAINAGE']
-row_in_dep['KMStoneToB'] = 'NA'
-row_in_dep['SuveryDist'] = porsa_ds['DISTENCE(M)']
-row_in_dep['LatAuth'] = porsa_ds['START_COORDINATE'].apply(find_lat)
-row_in_dep['LongAuth'] = porsa_ds['START_COORDINATE'].apply(find_long)
-row_in_dep['LandmarkRHS'] = porsa_ds['POINT NAME'].apply(landmark)
-row_in_dep['VlgTwnPoint'] = porsa_ds['ROAD NAME']
-row_in_dep['OFClaying'] = porsa_ds['OFC POSITION']
-row_in_dep['RdCrossing Req'] = porsa_ds['ROAD NAME'].apply(road_xing_req)
-row_in_dep['ReasonRdCrossing'] = porsa_ds['ROAD NAME'].apply(road_xing_reas)
-row_in_dep['UtilityLHS'] = porsa_ds.apply(finding_utility_lhs, axis=1)
-row_in_dep['UtilityRHS'] = porsa_ds.apply(finding_utility_rhs, axis=1)
-row_in_dep['UtilityChecked'] = porsa_ds.apply(utility_checked, axis=1)
-row_in_dep['RowAuthorityName'] = porsa_ds.apply(road_authority, axis=1)
-row_in_dep['AuthorityAddress'] = porsa_ds.apply(road_auth_add, axis=1)
+row_in_dep['KMStoneFromA'] = 'NA'
+row_in_dep['KMStoneToB'] = _ds['ROAD CHAINAGE']
+row_in_dep['SuveryDist'] = _ds['DISTENCE(M)']
+row_in_dep['LatAuth'] = _ds['START_COORDINATE'].apply(find_lat)
+row_in_dep['LongAuth'] = _ds['START_COORDINATE'].apply(find_long)
+row_in_dep['LandmarkRHS'] = _ds['POINT NAME'].apply(landmark)
+row_in_dep['VlgTwnPoint'] = _ds['ROAD NAME']
+row_in_dep['OFClaying'] = _ds['OFC POSITION']
+row_in_dep['RdCrossing Req'] = _ds['ROAD NAME'].apply(road_xing_req)
+row_in_dep['ReasonRdCrossing'] = _ds['ROAD NAME'].apply(road_xing_reas)
+row_in_dep['UtilityLHS'] = _ds.apply(finding_utility_lhs, axis=1)
+row_in_dep['UtilityRHS'] = _ds.apply(finding_utility_rhs, axis=1)
+row_in_dep['UtilityChecked'] = _ds.apply(utility_checked, axis=1)
+row_in_dep['RowAuthorityName'] = _ds.apply(road_authority, axis=1)
+row_in_dep['AuthorityAddress'] = _ds.apply(road_auth_add, axis=1)
 row_in_dep['FeasibilityOfROWApproval'] = 'Yes'
 row_in_dep['TypeOfOverlapArea'] = 'NA'
-row_in_dep['NearestLandmark'] = porsa_ds['POINT NAME'].apply(nearest_landmark)
+row_in_dep['NearestLandmark'] = _ds['POINT NAME'].apply(nearest_landmark)
 row_in_dep['LengthOfOverlapArea'] = 'NA'
 row_in_dep['ExpansionInProg'] = 'NA'
 row_in_dep['ExpansionPlanned'] = 'NA'
-row_in_dep['TypeOfCrossing'] = porsa_ds.apply(calculate_structure, axis=1)
-row_in_dep['LatCrossing'] = porsa_ds.apply(structure_xing_lat, axis=1)
-row_in_dep['LongCrossing'] = porsa_ds.apply(structure_xing_long, axis=1)
-row_in_dep['LatLandmark'] = porsa_ds['END_COORDINATE'].apply(landmark_lat)
-row_in_dep['LongLandmark'] = porsa_ds['END_COORDINATE'].apply(landmark_long)
-row_in_dep['LengthOfCrossing'] = porsa_ds['LENGTH (IN Mtr.)']
-row_in_dep['SoilType'] = porsa_ds['SOIL TYPE']
+row_in_dep['TypeOfCrossing'] = _ds.apply(calculate_structure, axis=1)
+row_in_dep['LatCrossing'] = _ds.apply(structure_xing_lat, axis=1)
+row_in_dep['LongCrossing'] = _ds.apply(structure_xing_long, axis=1)
+row_in_dep['LatLandmark'] = _ds['END_COORDINATE'].apply(landmark_lat)
+row_in_dep['LongLandmark'] = _ds['END_COORDINATE'].apply(landmark_long)
+row_in_dep['LengthOfCrossing'] = _ds['LENGTH (IN Mtr.)']
+row_in_dep['SoilType'] = _ds['SOIL TYPE']
 row_in_dep['RouteRdSidePts'] = 'NA'
 row_in_dep['AlternateReq'] = 'NA'
 row_in_dep['AlternateProp'] = 'NA'
 row_in_dep['AlternatePathLgh'] = 'NA'
 row_in_dep['Remarks'] = ''
 
-with pd.ExcelWriter('excel_files/Porsa Block/Porsa_RoW.xlsx', engine='openpyxl', mode='w') as writer:
+with pd.ExcelWriter('excel_files/Tarana Block/Tarana_RoW.xlsx', engine='openpyxl', mode='w') as writer:
     row_in_dep.to_excel(writer, sheet_name='PreSurvey', index=False)
