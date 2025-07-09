b3575b66f7292ac2a7e465526aa66fad2822d052
sks092016 <subhashsoni403@gmail.com>
2025-07-09 09:20:42 +0530

    Chnaged the complete structure

--- generating_the_ds.py
+++ generating_the_ds.py
@@ -210,6 +210,8 @@ def calculate_protec(row, param):
     elif strata == 'hard_rock':
         if param == 'for':
             return 'Hard Rock'
+        if param == 'type':
+            return hard_rock
         if param == 'length':
             return length + 6
     return ''
@@ -333,36 +335,38 @@ with pd.ExcelWriter(str(dir_path)+f"\\{districtName}-{blockName}-{version}.xlsx"
 ####################################### Generating the Protection Details ###########################################
 
 cols = ['ROUTE NAME','ROUTE TYPE','TOTAL ROUTE LENGTH','NO OF CULVERT','LENGTH (IN Mtr) OF CULVERT','NO OF BRIDGE','LENGTH (IN Mtr) OF BRIDE',
-'LENGTH (IN Mtr) OF PCC','LENGTH (IN Mtr) OF DWC','LENGTH (IN Mtr) OF GI','LENGTH (IN Mtr) OF DWC+PCC',"Soil Detail", 	"LENGTH (IN Mtr) OF DWC+PCC (HARD ROCK)",'LENGTH (IN Mtr) OF ANCORING']
+'LENGTH (IN Mtr) OF PCC','LENGTH (IN Mtr) OF DWC','LENGTH (IN Mtr) OF GI','LENGTH (IN Mtr) OF DWC+PCC',"Soil Detail", "LENGTH (IN Mtr) OF DWC+PCC (HARD ROCK)",'LENGTH (IN Mtr) OF ANCORING']
 protection_details = pd.DataFrame(columns = cols)
 
 _details = pd.read_excel(str(dir_path)+f"\\{districtName}-{blockName}-{version}.xlsx", sheet_name='Details Sheet')
 
 _protection = _details[['ROUTE NAME', 'ROAD STRUTURE TYPE', 'LENGTH (IN Mtr.)', 'PROTECTION TYPE','PROTECTION FOR', 'PROTECTION LENGTH (IN Mtr.)']]
-r_agg = _protection.groupby(['ROUTE NAME', 'ROAD STRUTURE TYPE'])['LENGTH (IN Mtr.)'].agg(['count', 'sum']).unstack(fill_value=0)
-r_agg.columns = ['NO OF BRIDGE','NO OF CULVERT', 'LENGTH (IN Mtr) OF BRIDGE','LENGTH (IN Mtr) OF CULVERT']  # Rename columns
+r_agg = _protection.groupby(['ROUTE NAME', 'PROTECTION FOR'])['PROTECTION LENGTH (IN Mtr.)'].agg(['count', 'sum']).unstack(fill_value=0)
+r_agg.columns = ['NO OF BRIDGE','NO OF CULVERT','HARD ROCK','LENGTH (IN Mtr) OF BRIDGE','LENGTH (IN Mtr) OF CULVERT', 'HARD ROCK (Length)']  # Rename columns
 r_agg = r_agg.reset_index()
 p_agg = _protection.pivot_table(values='PROTECTION LENGTH (IN Mtr.)', index='ROUTE NAME', columns='PROTECTION TYPE', aggfunc='sum', fill_value=0).reset_index()
 protection_ = pd.merge(r_agg, p_agg, on= 'ROUTE NAME', how='outer')
 
+merged_df = boq_sd_df
 merged_df.rename(columns={'span_name' : 'ROUTE NAME'} ,inplace=True)
 protection_ = pd.merge(protection_, merged_df, on='ROUTE NAME', how='outer')
 
 protection_details['ROUTE NAME'] = protection_['ROUTE NAME']
-protection_details['ROUTE TYPE'] = protection_['scope']
+protection_details['ROUTE TYPE'] = 'OFC to be laid for Ring Formation (in Km)'
 protection_details['TOTAL ROUTE LENGTH'] = protection_['distance']
 protection_details['NO OF CULVERT'] = protection_['NO OF CULVERT']
-protection_details['LENGTH (IN Mtr) OF CULVERT'] = protection_['LENGTH (IN Mtr) OF CULVERT']
+protection_details['LENGTH (IN Mtr) OF CULVERT'] = protection_['LENGTH (IN Mtr) OF CULVERT'] - 6 * protection_['NO OF CULVERT']
 protection_details['NO OF BRIDGE'] = protection_['NO OF BRIDGE']
-protection_details['LENGTH (IN Mtr) OF BRIDE'] = protection_['LENGTH (IN Mtr) OF BRIDGE']
+protection_details['LENGTH (IN Mtr) OF BRIDE'] = protection_['LENGTH (IN Mtr) OF BRIDGE'] - 6 * protection_['NO OF BRIDGE']
 protection_details['LENGTH (IN Mtr) OF PCC'] = 0
-protection_details['LENGTH (IN Mtr) OF DWC'] = protection_['NO OF CULVERT'] * 6 + protection_['LENGTH (IN Mtr) OF CULVERT']
-protection_details['LENGTH (IN Mtr) OF GI'] = protection_['NO OF BRIDGE'] * 6 + protection_['LENGTH (IN Mtr) OF BRIDGE']
-protection_details['LENGTH (IN Mtr) OF DWC+PCC'] = protection_['DWC+PCC']
-
+protection_details['LENGTH (IN Mtr) OF DWC'] = protection_['LENGTH (IN Mtr) OF CULVERT']
+protection_details['LENGTH (IN Mtr) OF GI'] = protection_['LENGTH (IN Mtr) OF BRIDGE']
+protection_details['LENGTH (IN Mtr) OF DWC+PCC'] = protection_['HARD ROCK (Length)']
+protection_details['Soil Details'] = ''
+protection_details['HARD ROCK (Length)'] = protection_['HARD ROCK (Length)']
 protection_details['LENGTH (IN Mtr) OF ANCORING'] = 0
 
-with pd.ExcelWriter('References/Porsa Block/Porsa-BoQ.xlsx', engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+with pd.ExcelWriter(str(dir_path)+f"\\{districtName}-{blockName}-{version}.xlsx", engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
     protection_details.to_excel(writer, sheet_name='Protection Details', index=False)
 #
 # ###########################################################################################################################################
