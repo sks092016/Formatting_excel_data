38c7df8a592e3529ae24162134f6aae907a54900
SubhashSoni <subhashsoni403@gmail.com>
2025-07-24 18:20:05 +0530

    updated main file

--- Drafting_the_The_Sequence.py
+++ Drafting_the_The_Sequence.py
@@ -5,7 +5,6 @@ version = "1.0"
 import pandas as pd
 
 gdf = gpd.read_file('References/Formats/OFC_NEW.shp')
-
 gdf_new = gdf.iloc[0:0].copy()
 gdf_new["Sequence"] = None
 
@@ -19,20 +18,12 @@ for s in span:
     temp_df = gdf[gdf.span_name == s].sort_values('Sequqnce')
     temp_df['start'] = temp_df.geometry.apply(lambda geom: get_start_end_coords(geom)[0])
     temp_df['end'] = temp_df.geometry.apply(lambda geom: get_start_end_coords(geom)[1])
-
-
     sorted_indices = []
-
-
     remaining = temp_df.copy()
-
-
     current_idx = remaining.index[0]
     current = remaining.loc[current_idx]
     sorted_indices.append(current_idx)
     remaining = remaining.drop(current_idx)
-
-
     while not remaining.empty:
         found = False
         for idx, row in remaining.iterrows():
@@ -45,20 +36,11 @@ for s in span:
         if not found:
             print("Warning: A disconnected segment remains. Stopping sequence generation.")
             break
-
-
     sequence_series = temp_df.index.to_series().apply(
         lambda x: sorted_indices.index(x) + 1 if x in sorted_indices else None)
-
-
     temp_df['Sequence'] = sequence_series
-
-
     temp_df = temp_df.drop(columns=['start', 'end'])
-
     assert gdf_new.crs == temp_df.crs, "CRS mismatch!"
     gdf_new = pd.concat([gdf_new, temp_df])
-
 gdf_new.to_file(f'References/Output/OFC_NEW_{version}.shp')
-
 print("Sequence assigned and saved with original index preserved.")
