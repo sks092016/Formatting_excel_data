ea42bd35a1287e069a08a3dccde0e7368b3f5783
SubhashSoni <subhashsoni403@gmail.com>
2025-08-21 15:46:31 +0530

    sequenced bina

--- Generating SLDs/generate_roW_shp.py
+++ Generating SLDs/generate_roW_shp.py
@@ -5,7 +5,7 @@ import json
 from shapely.ops import transform
 import pyproj
 
-block = "Budhni"
+block = "Multai"
 def line_length_meter(line):
     geod = Geod(ellps="WGS84")
     length_m = geod.line_length(
