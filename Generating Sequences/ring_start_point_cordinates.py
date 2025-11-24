a503cca7fbb56f3c51ed5a321f8e09a47bceb87a
sks092016 <subhashsoni403@gmail.com>
2025-11-24 22:17:30 +0530

    sequenced Nalkheda

--- Generating Sequences/ring_start_point_cordinates.py
+++ Generating Sequences/ring_start_point_cordinates.py
@@ -7,7 +7,7 @@ from datetime import datetime
 log_dir = Path("logs")
 log_dir.mkdir(exist_ok=True)
 
-blockName = "Neemuch"
+blockName = "Dabra"
 
 now = datetime.now()
 formatted = now.strftime("%d-%m-%y_%H-%M-%S")
@@ -32,30 +32,32 @@ logging.getLogger('').addHandler(console)
 #### Checking the CRS of the shape file
 
 # input file names
-gps_shape_file = "References/Neemuch/gps.shp"
-segments_shape_file = "References/Neemuch/OFC_NEW.shp"
+gps_shape_file = "References/Dabra/gps.shp"
+segments_shape_file = "References/Dabra/OFC_NEW.shp"
 
 version = f"{blockName}-1"
 
 ## The Start cordinate for main Rings is BHQ ##
-BHQ_CORDINATE = '74.86450370 24.46730560'
+BHQ_CORDINATE = '78.33176240 25.88569860'
 
 rings = {
     'R1': BHQ_CORDINATE,
     'R2': BHQ_CORDINATE,
     'R3': BHQ_CORDINATE,
     'R4': BHQ_CORDINATE,
-    'R1-C1':'74.81378274 24.49133623',
-    'R1-C2':'74.87854117 24.48335118',
-    'R2-C1':'74.87770374 24.31460132',
-    'R3-C1':'74.98572116 24.44235594',
-    'R4-C1':'74.93271743 24.37387269',
+    'R5': BHQ_CORDINATE,
+    'R2-C1':'78.44659280 26.05667590',
+    'R3-C1':'78.28074915 25.99036729',
+    'R3-C2':'78.34723653 26.03645987',
+    'R4-C1':'78.24909173 25.89715821',
 }
 t_point_ring_spans = {
-'T-POINT BAMORI':(74.87770374,24.31460132),
-'T-POINT BHATKHEDA':(74.93271743,24.37387269),
-'T-POINT BISALWASSONGARA':(74.98572116,24.44235594),
-'T-POINT DHANIRAKALAN':(74.81378274,24.49133623),
-'T-POINT HARWAR':(74.92378449,24.33765014),
-'T-POINT KANAVATI':(74.87854117,24.48335118),
+'T-POINT BIJAKPUR':(78.27605533,25.82013164),
+'T-POINT JANAKPUR':(78.42298810,26.03554120),
+'T-POINT KIROL':(78.55211430,26.05305950),
+'T-POINT LAKHNOTI':(78.44659280,26.05667590),
+'T-POINT MAHARAJPUR':(78.24909173,25.89715821),
+'T-POINT TEKANPUR':(78.28074915,25.99036729),
+'T-POINT LADERA':(78.34717654,26.03648567),
+'T-POINT DABRA BLOCK':(78.30759524,25.84339944),
 }
