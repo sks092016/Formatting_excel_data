d0515171ca5922418839aa5e3120a402aaf7b30f
SubhashSoni <subhashsoni403@gmail.com>
2025-12-09 17:40:40 +0530

    sequenced Completed the Joint Sequencing in UP

--- Generating Sequences/qgis_conditional_replacement_substring.py
+++ Generating Sequences/qgis_conditional_replacement_substring.py
@@ -1,13 +1,13 @@
 from qgis.core import QgsProject, edit
 
 # --- SETTINGS ---
-layer_name = "OFC_NEW"  # Name of the layer in QGIS
-check_field = "span_name"
-update_field = "span_name"
+layer_name = "OFC_New_Shajapur-1_Seg_Span_Seq"  # Name of the layer in QGIS
+check_field = "ofc_laying"
+update_field = "ofc_laying"
 
 
 replacement_map = {
-'T-POINT SARAY TO UMARPURA':'T-POINT NALCHA TO UMARPURA'
+'Brown Filed Data':'Brown Field Data'
 }
 count = 0
 
