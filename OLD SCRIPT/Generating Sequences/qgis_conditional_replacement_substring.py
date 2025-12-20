5cd579febe48e7ac294faa8db37a18d4eec03f99
sks092016 <subhashsoni403@gmail.com>
2025-12-20 23:20:16 +0530

    sequenced Athner

---
+++ OLD SCRIPT/Generating Sequences/qgis_conditional_replacement_substring.py
@@ -0,0 +1,36 @@
+from qgis.core import QgsProject, edit
+
+# --- SETTINGS ---
+layer_name = "OFC_NEW"  # Name of the layer in QGIS
+check_field = "span_name"
+update_field = "span_name"
+
+
+replacement_map = {
+'ISAKPURKHEDI':'ISAKPUR KHEDI', 
+'KHAIRWAJAGIR':'KHAIRWA JAGIR', 
+'PRATAPPARU DHAVHIYA':'PRATAPPRU DHAVHIYA'
+}
+count = 0
+
+# --- SCRIPT ---
+layer = QgsProject.instance().mapLayersByName(layer_name)[0]
+if not layer:
+    raise Exception(f"Layer '{layer_name}' not found!")
+
+with edit(layer):
+    for feature in layer.getFeatures():
+        check_value = feature[check_field]
+        if not check_value:
+            continue
+        for key, value in replacement_map.items():
+            if key in check_value:  # case-sensitive; use lower() if needed
+                new_value = check_value.replace(key, value)
+                count += 1
+                layer.changeAttributeValue(
+                    feature.id(),
+                    layer.fields().indexFromName(update_field),
+                    new_value
+                )
+            
+print(f"✅ {count} replacements completed successfully!")
